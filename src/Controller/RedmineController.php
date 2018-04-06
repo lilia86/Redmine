7acec6823a7a3d1f67c2565d3c5edb121f7a5023
lilia86 <liliagirl@mail.ru>
2018-04-06 14:54:42 +0300

    Libraries

---
+++ src/Controller/RedmineController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+
+class RedmineController extends Controller
+{
+    /**
+     * @Route("/lucky/number")
+     */
+    public function number()
+    {
+
+    }
+}
---
+++ src/Service/ApiRequestManager.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Service;
+
+
+use GuzzleHttp\Client;
+
+
+class ApiRequestManager
+{
+    /**
+     *
+     */
+    public function number()
+    {
+
+
+    }
+}
